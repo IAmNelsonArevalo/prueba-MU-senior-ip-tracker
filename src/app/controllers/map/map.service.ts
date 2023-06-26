f62cb9497bb8816a3500735eb8f11835cfd4a4b9
Nelson Arevalo <nelson.arevalo2119@gmail.com>
2023-06-26 11:16:51 -0500

    Map 35%

---
+++ src/app/controllers/map/map.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+/** Interfaces & Types */
+import type {ILocationData} from "src/app/models/interfaces/map.interfaces";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MapService {
+    constructor(
+        private http: HttpClient
+    ) {
+    }
+
+    public getLocation(): Observable<ILocationData> {
+        return new Observable((observer) => {
+            navigator.geolocation.getCurrentPosition(
+                (position: GeolocationPosition) => {
+                    const locationData: ILocationData = {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    };
+
+                    observer.next(locationData);
+                    observer.complete();
+                },
+                (error: GeolocationPositionError) => {
+                    observer.error(error);
+                }
+            );
+        })
+    }
+
+    public getIpAddress(): Observable<Object> {
+        return this.http.get(`https://api.seeip.org/jsonip`)
+    }
+
+    public getAddressItems(latitude: number, longitude: number): Observable<Object> {
+        return this.http.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`);
+    }
+
+    public getTimezoneAndIPS(ip: string): any {
+        return this.http.get(`https://ipapi.co/${ip}/json/`)
+    }
+}
